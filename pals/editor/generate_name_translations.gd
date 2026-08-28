6a791f74b289200a2aa189c1483ae557c1724f59
Pheubel <luca.guagliardo@live.nl>
2026-08-28 15:40:13 +0200

    update translation

--- pals/editor/generate_name_translations.gd
+++ pals/editor/generate_name_translations.gd
@@ -4,20 +4,26 @@
 extends EditorScript
 
 const DEFINITION_DIR: String = "res://pals/definitions/"
-const OUTPUT_FILE := "res://i10n/pal_names.csv"
-
+const OUTPUT_POT: String = "res://i10n/pal_names.pot"
 
 func _run() -> void:
-	var name_file := FileAccess.open(OUTPUT_FILE, FileAccess.WRITE)
-	if name_file == null:
+	var pot_file := FileAccess.open(OUTPUT_POT, FileAccess.WRITE)
+	if pot_file == null:
 		push_error(error_string(FileAccess.get_open_error()))
 		return
-	
-	name_file.store_csv_line(["keys", "en", "ja"])
 	
 	for file in DirAccess.get_files_at(DEFINITION_DIR):
 		var pal_definition := load(DEFINITION_DIR.path_join(file)) as PalResource
 		
-		name_file.store_csv_line([pal_definition.translation_key, pal_definition.resource_name])
+		if not pal_definition:
+			push_error("could not load '%s' as PalResource" % file)
+			continue
+		
+		#NOTE: this intermediate step is needed to avoid an editor crash
+		var translation_key: String = pal_definition.translation_key
+		
+		pot_file.store_line("msgid \"%s\"" % translation_key)
+		pot_file.store_line("msgstr \"\"")
+		pot_file.store_line("")
 	
-	name_file.close()
+	pot_file.close()
