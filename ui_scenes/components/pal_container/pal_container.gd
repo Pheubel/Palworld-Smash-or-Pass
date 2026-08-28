0883565f6510267d8f9eca6adc0612a8e3b2a17e
Pheubel <luca.guagliardo@live.nl>
2026-08-28 18:20:59 +0200

    make pal container open pal inpection scene

--- ui_scenes/components/pal_container/pal_container.gd
+++ ui_scenes/components/pal_container/pal_container.gd
@@ -3,12 +3,16 @@ extends PanelContainer
 
 signal pressed
 
+const INSPECT_PAL_SCENE = preload("uid://bt20nqxbax1sy")
 
 @onready var pal_icon: TextureRect = %PalIcon
 @onready var pal_name_label: Label = %PalNameLabel
 
+var _pal: PalResource
 
 func prepare(pal: PalResource) -> void:
+	_pal = pal
+	
 	if not is_node_ready():
 		await ready
 	
@@ -18,3 +22,9 @@ func prepare(pal: PalResource) -> void:
 
 func _on_button_pressed() -> void:
 	pressed.emit()
+	
+	var inspect_pal_scene := INSPECT_PAL_SCENE.instantiate()
+	inspect_pal_scene.current_pal = _pal
+	inspect_pal_scene.exit_requested.connect(inspect_pal_scene.queue_free)
+	
+	get_tree().root.add_child(inspect_pal_scene)
